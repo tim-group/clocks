f81927917633d9de495e79ec7738aad782daa14e
Steve Haslam <steve.haslam@timgroup.com>
2019-09-05 10:57:46 +0100

    SteveH: also upgrade jarmangit to delay creation of tasks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        val repoUrl: String by settings
+        maven(url = "$repoUrl/groups/public")
+    }
+}
+
 rootProject.name = "clocks"
 
 include("joda")
