f0a89d7ce6ad8f734e60e5c1b81c0b51a5302070
Steve Haslam <steve.haslam@timgroup.com>
2019-12-17 15:07:47 +0000

    Bump Joda-Time to 2.10.5

--- joda/build.gradle.kts
+++ joda/build.gradle.kts
@@ -2,7 +2,7 @@ description = "Extension of java.time.Clock to integrate with Joda-Time"
 extra["javaModuleName"] = "com.timgroup.clocks.joda"
 
 dependencies {
-    api("joda-time:joda-time:2.10.4")
+    api("joda-time:joda-time:2.10.5")
     testImplementation("junit:junit:4.12")
     testImplementation("org.hamcrest:hamcrest-core:1.3")
     testImplementation("org.hamcrest:hamcrest-library:1.3")
